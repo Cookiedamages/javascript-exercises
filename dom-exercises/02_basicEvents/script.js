a6b9abbe0ccd23d4f3b52416e9ebf083a1b31e15
Cookiedamages <Cookiedamages3@gmail.com>
2023-05-31 23:35:23 +0400

    add 02_basicEvents exercise

---
+++ dom-exercises/02_basicEvents/script.js
@@ -0,0 +1,23 @@
+///--------Method 1----------///
+
+//  <button onclick="alert('Hello World')">Click Me</button>
+
+///--------Method 2----------///
+
+//  <!-- the HTML file -->  //
+//  <button id="btn">Click Me</button>
+
+//  in the JavaScript file  //
+//  const btn = document.querySelector('#btn');
+//  btn.onclick = () => alert("Hello World");
+
+///--------Method 3----------///
+
+//  <!-- the HTML file -->  //
+//  <button id="btn">Click Me Too</button>
+
+//  the JavaScript file  //
+//  const btn = document.querySelector('#btn');
+//  btn.addEventListener('click', () => {
+//    alert("Hello World");
+//  });
